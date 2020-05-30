ee319f12124ed6b218545e33596c0630383fe33e
justcode-srihitha <60424461+justcode-srihitha@users.noreply.github.com>
2020-05-30 15:21:17 +0530

    Add add_sub funtions
    Add funtions which perform simple Addition and subtraction

---
+++ gps_health_daily.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import math
+
+class Addition_subtraction(object):
+    def __init__(a,s):
+        self.a = a
+        self.s = s
+ 
+def add_sub_actual()->Addition_subtraction:
+    x=9
+    y=10
+    a=x+y
+    s=abs(x-y)
+    print('HI')
+    return (a,s)
+        
+    
+
